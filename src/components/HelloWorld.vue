fde2797bcb0ff3ee0576fdfeff1ff28c9aa1c65a
= <samuel@tedbree.com>
2019-11-09 02:58:04 +0100

    Initial Commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Hello World</h1>
+        <p>Welcome to my First Vue Component Package. Thanks</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
